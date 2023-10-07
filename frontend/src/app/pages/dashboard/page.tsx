811a1f775babea1c5324fc665478d812914407fc
HACKATHON UNIFESO <HACKATHON@acad.unifeso.lan>
2023-10-07 14:59:36 -0300

    Dashboard

---
+++ frontend/src/app/pages/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Table from "@/app/components/table";
+
+export default function Example() {
+    return (
+        <div>
+          <Table />
+          <p>Teste</p>
+          <Table />
+        </div>
+    )
+  }
+  
