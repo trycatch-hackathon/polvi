d362b514030dc7c1f7db5d2b7e9db19f51bf9e1e
HACKATHON UNIFESO <HACKATHON@acad.unifeso.lan>
2023-10-08 08:51:40 -0300

    removendo arquivos

--- frontend/src/app/pages/dashboard/page.tsx
+++ frontend/src/app/pages/dashboard/page.tsx
@@ -1,6 +1,6 @@
-import Table from "../../../app/components/Table";
-import Navbar from "../../../app/components/navbar";
-import Map from "../../../app/components/Map";
+import Table from "../../components/Table";
+import Navbar from "../../components/navbar";
+import Map from "../../components/Map";
 
 
 export default function Example() {
