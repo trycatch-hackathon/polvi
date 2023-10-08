58b0019940139d7488aea647d8a7422d47442308
Paulo Eduardo <dudu.magnificat@gmail.com>
2023-10-08 01:36:30 -0300

    salvando multer

--- backend/src/service/user/index.ts
+++ backend/src/service/user/index.ts
@@ -1,11 +1,39 @@
+import path from "path";
 import { UserRepository } from "../../repository/user";
+import { Storage } from "@google-cloud/storage";
+import Multer  from "multer"
+import { format } from 'util'
 
 class UserService {
   repository = new UserRepository();
-
+  
   async createOne(data: any) {
-    const result = await this.repository.createOne(data);
-    return result;
+
+    // const result = await this.repository.createOne(data);
+    // salvar foto no google cloud
+
+    const multer = Multer({
+      storage: Multer.memoryStorage(),
+      limits: {
+        fileSize: 10 * 1024 * 1024, 
+      },
+    });
+
+    const gCloud = new Storage({
+      keyFilename: path.join(__dirname, '../../googlecloud/hip-gecko-401402-1bd276f77609.json'),
+      projectId: 'hip-gecko-401402'
+    })
+
+    const irregularBucket = gCloud.bucket('irregular-app');
+
+    multer.single(data.avatarUrl)
+
+    const blob = irregularBucket.file(data.avatarUrl);
+    const blobStream = blob.createWriteStream();
+  
+    const publicUrl = format(`https://storage.googleapis.com/${irregularBucket.name} / ${blob.name} `);
+    
+    return publicUrl;
   }
 
   async findUnique(query: any) {
