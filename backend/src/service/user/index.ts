7c4b1b6975370a80c2e9cdd55bb9decd8fbfa13a
mateussatoh <mateussatoh@gmail.com>
2023-10-08 01:30:21 -0300

    repository trocado

--- backend/src/service/user/index.ts
+++ backend/src/service/user/index.ts
@@ -3,13 +3,13 @@ import { UserRepository } from "../../repository/user";
 class UserService {
   repository = new UserRepository();
 
-  async login(data: any) {
-    const result = await this.repository.createOne(data);
+  async login(query: any) {
+    const result = await this.repository.findUnique(query);
     return result;
   }
 
-  async signup(query: any) {
-    const result = await this.repository.findUnique(query);
+  async signup(data: any) {
+    const result = await this.repository.createOne(data);
     return result;
   }
 }
