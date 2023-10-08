6e1021216bb3edbc91d159943b6d3d882e0bcfc9
HACKATHON UNIFESO <HACKATHON@acad.unifeso.lan>
2023-10-08 05:06:25 -0300

    Card ocorrência

--- frontend/src/app/components/Modal/rowUsuario.jsx
+++ frontend/src/app/components/Modal/rowUsuario.jsx
@@ -1,6 +1,6 @@
 export default function RowUsuario(props) {
     return (
-        <div className="bg-white rounded-lg p-6">
+        <div className="rounded-lg p-6">
             <img src={props.person.image} className="h-16 w-16 rounded-full mx-auto" />
             <div>
                 <h2>{props.person.name}</h2>
